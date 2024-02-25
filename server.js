851d54ad6fed9d70f828a42d039c68f3b6f95a5f
SaidOosman <saiidoosman@gmail.com>
2024-02-25 22:35:56 +0000

    start server

--- server.js
+++ server.js
@@ -302,6 +302,11 @@ app.use(function(req,res) {
 
 });
 
-app.listen(3000, function(){
-    console.log("App started on port 3000");
+//app.listen(3000, function(){
+//    console.log("App started on port 3000");
+//});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+ console.log("App started on port: " + port);
 });
